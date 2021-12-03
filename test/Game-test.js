da01e0c4233a172c2fa70b2147c0dd04ce19d685
Christine Rowland <fordo29@gmail.com>
2021-12-02 19:37:47 -0700

    Begin to reefactor and lint

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,25 +1,12 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Turn');
-const Card = require('../src/Card');
-const Deck = require('../src/Deck');
-const data = require('../src/test-data');
-const Round = require('../src/Round');
 const Game = require('../src/Game');
 
 describe('Game Class Properties', function() {
-  let card;
-  let turn;
-  let deck;
-  let round;
   let game;
 
   beforeEach(function() {
-    // card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    // turn = new Turn('object', card);
-    deck = new Deck(data.prototypeTestData);
-    round = new Round(deck);
     game = new Game();
   });
 
@@ -34,14 +21,4 @@ describe('Game Class Properties', function() {
   it('should have a function that starts the game', function() {
     expect(game.start).to.be.a('function');
   });
-
-  it.skip('should create cards at the start of the game', function() {
-    expect(game.currentRound).to.be.anInstanceOf(Round);
-  });
-
-  it.skip('should create a new round at the start of the game', function() {
-    game.start();
-    expect(game.round).to.be.instanceOf(Round);
-  });
-
 });
